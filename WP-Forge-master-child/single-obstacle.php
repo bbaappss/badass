99c68fd825fe07a09c271eb8c983a5ada08ec1a2
Brandon Scott <brandonscott@gmail.com>
2014-02-23 21:31:54 -0800

    Implement new slider

--- WP-Forge-master-child/single-obstacle.php
+++ WP-Forge-master-child/single-obstacle.php
@@ -15,33 +15,15 @@
             $videoEmbed = get_post_meta($post->ID, 'video_embed_code', true);
             $slideImages = get_post_meta($post->ID, 'obstacle_slide');
             $k9Check = get_post_meta($post->ID, 'k_9_obstacle');
+            $slider = get_post_meta($post->ID, 'slider', true)
         ?>
+        
         <h2 class="font-messy">
             <?php echo $name; ?>
         </h2>
-
-        <?php $boolCheck = $slideImages['0']; ?>
-
-        <?php if ( !empty($videoEmbed)  || !is_bool($boolCheck) )   : ?>
-            <ul class="orbit-obstacle" data-orbit>
-                <?php if ( !empty($videoEmbed) )  : ?>
-                    <li>
-                        <iframe style="width: 100%; height: 100%; z-index: 100" src="//www.youtube.com/embed/<?php echo $videoEmbed; ?>" frameborder="0" allowfullscreen></iframe>
-                    </li>
-                <?php endif; ?>
-                <?php if (!is_bool($boolCheck))  : ?>
-                    <?php foreach ( $slideImages as $attachment ): ?>
-                    <li>
-                        <div class="cropped-image-slide" 
-                             style="background-image: url('<?php echo pods_image_url($attachment, 'large'); ?>');">
-                            <img src="<?php echo pods_image_url($attachment, 'medium'); ?>" />
-                        </div>
-                    </li>
-                    <?php endforeach; ?>
-                <?php endif; ?>
-            </ul>
-        <?php endif; ?>
         
+        <?php putRevSlider( "$slider" ) ?>
+
         <div class="description">
             <?php the_content(); ?>
         </div>
