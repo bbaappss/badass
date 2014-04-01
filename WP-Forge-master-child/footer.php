c2c354ad4babbd6224251740248026415e7dc781
Brandon Scott <brandonscott@gmail.com>
2014-04-01 07:10:19 -0700

    Implement markup for footer modal slider

--- WP-Forge-master-child/footer.php
+++ WP-Forge-master-child/footer.php
@@ -10,12 +10,7 @@
  * @since WP-Starter 1.0
  */
 ?>
-	</section><!-- #main .wrapper -->
-    
-	<?php
-        if ( ! is_404() )
-        get_sidebar( 'footer' );
-    ?>    
+	</section><!-- #main .wrapper -->  
         
 	<footer class="footer row" role="contentinfo">
         <ul class="columns small-12 large-8">
@@ -23,9 +18,57 @@
             <li><a href="/privacy-policy">Privacy Policy</a></li>
         </ul>
         <span class="columns small-12 large-4 copyright">Copyright 2012-2014 BADASS Dash, LLC</span>
+
 	</footer><!-- .row -->
     
-	</div><!-- #wrapper -->  
+	</div><!-- #wrapper -->
+
+    <?php 
+      $footerModalPod = pods( 'footer_modal');
+      $footerModalPod->find('name ASC'); 
+    ?>
+    <?php while ( $footerModalPod->fetch() ) : ?> 
+        <?php
+            $title      = $footerModalPod->field('name');
+            $content    = $footerModalPod->display('post_content');
+            $fm_url    = $footerModalPod->field( 'fm_url' );
+            $fm_image  = $footerModalPod->field( 'fm_background_image' );
+            $fm_image  = pods_image($fm_image, '');
+            $hide_title = $footerModalPod->field('fm_hide_title');
+
+            echo '<div class="footer-modal';              
+                  // include conditional classes
+                  if (!($fm_url=='')) {
+                    echo 'has-link ';
+                  }
+                  if (!($fm_image=='')) {
+                    echo 'has-bg-image ';
+                  }
+
+            echo '">';
+
+                echo '<div class="footer-modal-inner">';
+                    echo '<a href="#" class="no-go close-footer-modal close-modal">×</a>';
+
+                    if (!($fm_url=='')) {
+                        echo '<a href="'.$fm_url.'" class="link-this-block" target="_blank"></a>';
+                    }
+
+                    if (!($fm_image=='')) {
+                        echo'<div class="bg-image">'.$fm_image.'</div>';
+                    }
+
+                    echo '<div class="fm-content">';
+                        if (!($hide_title=='1')) {
+                            echo '<h2 class="font-messy">'.$title.'</h2>';
+                        }
+                        echo $content;
+                    echo '</div>';
+                echo '</div>';
+
+            echo '</div>';
+        ?>
+    <?php endwhile; ?>   
     
     <div id="backtotop">
     
@@ -34,6 +77,7 @@
     </div><!-- #backtotop -->  
 
 <?php wp_footer(); ?>
+
 <script type="text/javascript" async src="//assets.pinterest.com/js/pinit.js"></script>
 </body>
 </html>
