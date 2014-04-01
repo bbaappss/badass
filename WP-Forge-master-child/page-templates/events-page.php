9aa989362ca4067c0c26ae4bb0b731aefb6d1994
Brandon Scott <brandonscott@gmail.com>
2014-04-01 07:26:12 -0700

    Implement tabs on the events page to access the past events

--- WP-Forge-master-child/page-templates/events-page.php
+++ WP-Forge-master-child/page-templates/events-page.php
@@ -52,70 +52,80 @@ get_header(); ?>
         ?>
     <?php endwhile; ?>
 
-    <div class="columns small-12">
-        <h2>Upcoming</h2>
-        <div class="events-container">
-            <div class="row event-headings hide-for-small">
-                <span class="columns large-7 small-12 header-event-title">EVENT</span>
-                <span class="columns large-2 small-12 header-date">DATE</span>
-                <span class="columns large-3 small-12 header-register-now">REGISTER</span>
-            </div>
-            <?php foreach($futureEventPodArray as $futureEvent) : ?>
-                <?php
-                    $eventStringArray = explode(',,', $futureEvent);
-                    $title = $eventStringArray[0]; 
-                    $date = $eventStringArray[1]; 
-                    $linkToContent = $eventStringArray[2]; 
-                    $eventBriteUrl = $eventStringArray[3];
-                ?> 
-                <div class="row event">
-                    <div class="columns large-7 small-12 event-title-container">
-                        <a href="<?php echo $linkToContent ?>" class="event-title ba-btn font-messy">
-                                <?php echo $title; ?>
-                        </a>
-                    </div>
-                    <p class="columns large-2 small-12 date"><?php 
-                        $formattedDate = new DateTime($date);
-                        echo $formattedDate->format('F d, Y');
-                        ?>
-                    </p>
-                    <a class="columns large-3 small-12 register-now" href="<?php echo $eventBriteUrl; ?>" target="_blank">
-                        <img src="<?php bloginfo('stylesheet_directory'); ?>/images/eventbrite-custombutton.png" width="184" alt="Register Now"/>
-                    </a>
-                </div>
-            <?php endforeach; ?>
-        </div>
-        <h2>Past events</h2>
-        <div class="events-container">
-            <div class="row event-headings hide-for-small">
-                <span class="columns large-7 small-12 header-event-title">EVENT</span>
-                <span class="columns large-2 small-12 header-date">DATE</span>
-                <span class="columns large-3 small-12 header-register-now">REGISTER</span>
-            </div>
-            <?php foreach($pastEventPodArray as $pastEvent) : ?>
-            <?php
-                $eventStringArray = explode(',,', $pastEvent);
-                $title = $eventStringArray[0]; 
-                $date = $eventStringArray[1]; 
-                $linkToContent = $eventStringArray[2]; 
-                $eventBriteUrl = $eventStringArray[3];
-            ?> 
-            <div class="row event">
-                <div class="columns large-7 small-12 event-title-container">
-                    <a href="<?php echo $linkToContent ?>" class="event-title ba-btn font-messy">
-                            <?php echo $title; ?>
-                    </a>
-                </div>
-                <p class="columns large-2 small-12 date"><?php 
-                    $formattedDate = new DateTime($date);
-                    echo $formattedDate->format('F d, Y');
-                    ?>
+    <div class="columns small-12 events-page">
+        <div class="section-container tabs" data-section="tabs">
+            <section class="upcoming-events active">
+                <p class="title" data-section-title>
+                    <a href="#panel1">UPCOMING EVENTS</a>
                 </p>
-                <a class="columns large-3 small-12 register-now" href="<?php echo $eventBriteUrl; ?>" target="_blank">
-                    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/eventbrite-custombutton.png" width="184" alt="Register Now"/>
-                </a>
-            </div>
-        <?php endforeach; ?>
+                <div class="content" data-section-content>
+                    <div class="events-container">
+                        <div class="row event-headings hide-for-small">
+                            <span class="columns large-7 small-12 header-event-title">EVENT</span>
+                            <span class="columns large-2 small-12 header-date">DATE</span>
+                            <span class="columns large-3 small-12 header-register-now">REGISTER</span>
+                        </div>
+                        <?php foreach($futureEventPodArray as $futureEvent) : ?>
+                            <?php
+                                $eventStringArray = explode(',,', $futureEvent);
+                                $title = $eventStringArray[0]; 
+                                $date = $eventStringArray[1]; 
+                                $linkToContent = $eventStringArray[2]; 
+                                $eventBriteUrl = $eventStringArray[3];
+                            ?> 
+                            <div class="row event">
+                                <div class="columns large-7 small-12 event-title-container">
+                                    <a href="<?php echo $linkToContent ?>" class="event-title ba-btn font-messy">
+                                            <?php echo $title; ?>
+                                    </a>
+                                </div>
+                                <p class="columns large-2 small-12 date"><?php 
+                                    $formattedDate = new DateTime($date);
+                                    echo $formattedDate->format('F d, Y');
+                                    ?>
+                                </p>
+                                <a class="columns large-3 small-12 register-now" href="<?php echo $eventBriteUrl; ?>" target="_blank">
+                                    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/eventbrite-custombutton.png" width="184" alt="Register Now"/>
+                                </a>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            </section>
+            <section class="past-events">
+                <p class="title" data-section-title>
+                    <a href="#panel1">PAST EVENTS</a>
+                </p>
+                <div class="content" data-section-content>
+                    <div class="events-container">
+                        <div class="row event-headings hide-for-small">
+                            <span class="columns large-7 small-12 header-event-title">EVENT</span>
+                            <span class="columns large-2 small-12 header-date">DATE</span>
+                        </div>
+                        <?php foreach($pastEventPodArray as $pastEvent) : ?>
+                        <?php
+                            $eventStringArray = explode(',,', $pastEvent);
+                            $title = $eventStringArray[0]; 
+                            $date = $eventStringArray[1]; 
+                            $linkToContent = $eventStringArray[2]; 
+                            $eventBriteUrl = $eventStringArray[3];
+                        ?> 
+                        <div class="row event">
+                            <div class="columns large-7 small-12 event-title-container">
+                                <a href="<?php echo $linkToContent ?>" class="event-title ba-btn font-messy">
+                                        <?php echo $title; ?>
+                                </a>
+                            </div>
+                            <p class="columns large-2 small-12 date"><?php 
+                                $formattedDate = new DateTime($date);
+                                echo $formattedDate->format('F d, Y');
+                                ?>
+                            </p>
+                        </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            </section>
         </div>
     </div>
 </div><!-- #content -->
